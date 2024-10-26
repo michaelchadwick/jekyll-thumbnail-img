0c26b0bf907726f0ba25e89fe83f78ca7a5f730c
Abhishek Paudel <10024463+abpaudel@users.noreply.github.com>
2024-10-26 19:09:59 -0400

    generate thumbnails in _site

--- lib/jekyll-thumbnail-img.rb
+++ lib/jekyll-thumbnail-img.rb
@@ -3,56 +3,46 @@ require 'mini_magick'
 
 module Jekyll
   class JekyllThumbnailImg < Liquid::Tag
+    @@pending = [] # thumbnails to be generated
+
     def initialize(tag_name, markup, tokens)
       super
       @markup = markup
     end
 
     def render(context)
-      source, width = @markup.split(" ").map(&:strip)
+      source, width = @markup.split.map(&:strip)
+      source = context[source] if context[source]  # resolve liquid vars
 
-      # Check if the source is a Liquid variable and attempt to resolve it
-      if context[source]
-        source = context[source]
-      end
-      unless source && width
-        raise "Usage: {% thumbnail_img /path/to/local/image.png 500 %}"
-      end
+      raise "Usage: {% thumbnail_img /path/to/image.png 500 %}" unless source && width
+      raise "File #{source} not found" unless File.exist?(File.join(context.registers[:site].source, source))
 
-      source_path = File.join(context.registers[:site].source, source)
+      # Queue thumbnail for generation
+      dest = File.join(File.dirname(source), "thumbnails", "#{File.basename(source, '.*')}_#{width}w#{File.extname(source)}")
+      @@pending << {
+        source: File.join(context.registers[:site].source, source),
+        dest: File.join(context.registers[:site].dest, dest),
+        width: width
+      }
 
-      # Check if the source file exists
-      unless File.exist?(source_path)
-        raise "File #{source} could not be found"
-      end
-
-      # Calculate the relative path to the 'thumbnails' directory
-      relative_dest_dir = File.join(File.dirname(source), "thumbnails")
-
-      # Create the 'thumbnails' directory within the source directory
-      dest_dir = File.join(context.registers[:site].source, relative_dest_dir)
-      Dir.mkdir(dest_dir) unless Dir.exist?(dest_dir)
-
-      ext = File.extname(source)
-      dest = File.join(relative_dest_dir, File.basename(source, ext) + "_#{width}w#{ext}")
-
-      # Generate the thumbnail if it doesn't exist or if the source file was modified more recently than the thumbnail
-      full_dest_path = File.join(context.registers[:site].source, dest)
-      if !File.exist?(full_dest_path) || File.mtime(full_dest_path) <= File.mtime(source_path)
-        image = MiniMagick::Image.open(source_path)
-        image.resize("#{width}x")
-        image.write(full_dest_path)
-        Jekyll.logger.info "JekyllThumbnailImg", "Generating: " + File.join(context.registers[:site].source, dest)
-        context["site"]["static_files"] << StaticFile.new(
-          context.registers[:site],
-          context.registers[:site].source,
-          @dir,
-          dest
-        )
-      end
       dest
     end
+
+    def self.generate_thumbnails(site)
+      @@pending.each do |thumb|
+        FileUtils.mkdir_p(File.dirname(thumb[:dest]))
+        if !File.exist?(thumb[:dest]) || File.mtime(thumb[:dest]) <= File.mtime(thumb[:source])
+          Jekyll.logger.info "JekyllThumbnailImg:", "Generating: #{thumb[:dest].sub(site.dest + '/', '')}"
+          MiniMagick::Image.open(thumb[:source]).resize("#{thumb[:width]}x").write(thumb[:dest])
+        end
+      end
+      @@pending.clear
+    end
   end
+end
+
+Jekyll::Hooks.register :site, :post_write do |site|
+  Jekyll::JekyllThumbnailImg.generate_thumbnails(site)
 end
 
 Liquid::Template.register_tag("thumbnail_img", Jekyll::JekyllThumbnailImg)
