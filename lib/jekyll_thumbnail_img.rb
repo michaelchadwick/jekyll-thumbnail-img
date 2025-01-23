204802f15eab63cb156c88e844ae9d20e0d2e6e3
Michael Chadwick <michael.chadwick@gmail.com>
2025-01-22 17:05:57 -0800

    more style refactoring

--- lib/jekyll_thumbnail_img.rb
+++ lib/jekyll_thumbnail_img.rb
@@ -3,7 +3,7 @@ require 'mini_magick'
 
 module Jekyll
   class JekyllThumbnailImg < Liquid::Tag
-    @@pending = [] # thumbnails to be generated
+    @pending = [] # thumbnails to be generated
 
     def initialize(tag_name, markup, tokens)
       super
@@ -12,14 +12,14 @@ module Jekyll
 
     def render(context)
       source, width = @markup.split.map(&:strip)
-      source = context[source] if context[source]  # resolve liquid vars
+      source = context[source] if context[source] # resolve liquid vars
 
-      raise "Usage: {% thumbnail_img /path/to/image.png 500 %}" unless source && width
+      raise 'Usage: {% thumbnail_img /path/to/image.png 500 %}' unless source && width
       raise "File #{source} not found" unless File.exist?(File.join(context.registers[:site].source, source))
 
       # Queue thumbnail for generation
-      dest = File.join(File.dirname(source), "thumbnails", "#{File.basename(source, '.*')}_#{width}w#{File.extname(source)}")
-      @@pending << {
+      dest = File.join(File.dirname(source), 'thumbnails', "#{File.basename(source, '.*')}_#{width}w#{File.extname(source)}")
+      @pending << {
         source: File.join(context.registers[:site].source, source),
         dest: File.join(context.registers[:site].dest, dest),
         width: width
@@ -29,14 +29,14 @@ module Jekyll
     end
 
     def self.generate_thumbnails(site)
-      @@pending.each do |thumb|
+      @pending.each do |thumb|
         FileUtils.mkdir_p(File.dirname(thumb[:dest]))
         if !File.exist?(thumb[:dest]) || File.mtime(thumb[:dest]) <= File.mtime(thumb[:source])
-          Jekyll.logger.info "JekyllThumbnailImg:", "Generating: #{thumb[:dest].sub(site.dest + '/', '')}"
+          Jekyll.logger.info 'JekyllThumbnailImg:', "Generating: #{thumb[:dest].sub("#{site.dest}/", '')}"
           MiniMagick::Image.open(thumb[:source]).resize("#{thumb[:width]}x").write(thumb[:dest])
         end
       end
-      @@pending.clear
+      @pending.clear
     end
   end
 end
@@ -45,4 +45,4 @@ Jekyll::Hooks.register :site, :post_write do |site|
   Jekyll::JekyllThumbnailImg.generate_thumbnails(site)
 end
 
-Liquid::Template.register_tag("thumbnail_img", Jekyll::JekyllThumbnailImg)
+Liquid::Template.register_tag('thumbnail_img', Jekyll::JekyllThumbnailImg)
