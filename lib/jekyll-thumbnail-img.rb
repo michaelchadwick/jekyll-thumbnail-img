6d9fb19bd9d92f4cfbf913561dbe69b7f02a51ae
S8A <samuelochoap@gmail.com>
2025-01-27 13:39:39 -0400

    Generate thumbnails in cache directory to avoid needless regeneration

--- lib/jekyll-thumbnail-img.rb
+++ lib/jekyll-thumbnail-img.rb
@@ -17,13 +17,20 @@ module Jekyll
       width = context[width] if context[width]
 
       raise "Usage: {% thumbnail_img /path/to/image.png 500 %}" unless source && width
-      raise "File #{source} not found" unless File.exist?(File.join(context.registers[:site].source, source))
+
+      site = context.registers[:site]
+      raise "File #{source} not found" unless File.exist?(File.join(site.source, source))
 
       # Queue thumbnail for generation
-      dest = File.join(File.dirname(source), "thumbnails", "#{File.basename(source, '.*')}_#{width}w#{File.extname(source)}")
+      thumb_filename = "#{File.basename(source, '.*')}_#{width}w#{File.extname(source)}"
+      thumb_dir = File.join('.thumbnails', File.dirname(source), 'thumbnails')
+      dest = File.join(File.dirname(source), "thumbnails", thumb_filename)
+
+      # Add to pending list if not already there
       @@pending << {
-        source: File.join(context.registers[:site].source, source),
-        dest: File.join(context.registers[:site].dest, dest),
+        source: File.join(site.source, source),
+        cache: File.join(site.source, thumb_dir, thumb_filename),
+        dest: File.join(site.dest, dest),
         width: width
       } unless @@pending.any? { |thumb| thumb[:source] == source && thumb[:width] == width }
 
@@ -32,11 +39,17 @@ module Jekyll
 
     def self.generate_thumbnails(site)
       @@pending.each do |thumb|
+        FileUtils.mkdir_p(File.dirname(thumb[:cache]))
         FileUtils.mkdir_p(File.dirname(thumb[:dest]))
-        if !File.exist?(thumb[:dest]) || File.mtime(thumb[:dest]) <= File.mtime(thumb[:source])
+
+        # Generate thumbnail if not already generated or source is newer
+        if !File.exist?(thumb[:cache]) || File.mtime(thumb[:cache]) <= File.mtime(thumb[:source])
           Jekyll.logger.info "JekyllThumbnailImg:", "Generating: #{thumb[:dest].sub(site.dest + '/', '')}"
-          MiniMagick::Image.open(thumb[:source]).resize("#{thumb[:width]}x").write(thumb[:dest])
+          MiniMagick::Image.open(thumb[:source]).resize("#{thumb[:width]}x").write(thumb[:cache])
         end
+
+        # Copy from cache to destination
+        FileUtils.cp(thumb[:cache], thumb[:dest])
       end
       @@pending.clear
     end
